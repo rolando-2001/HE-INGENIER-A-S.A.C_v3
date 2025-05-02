c5fdeac5a47ca35b3465c0d3e39c396908a1237a
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2025-05-02 15:49:05 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig, envField } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import cloudflare from '@astrojs/cloudflare';
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      MAILER_SERVICE: envField.string({ context: "server", access: "secret" }),
+      MAILER_EMAIL: envField.string({ context: "server", access: "secret" }),
+      MAILER_SECRET_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+
+  output: 'server',
+  adapter: cloudflare(),
+  experimental: {
+    session: true, 
+  }
+});
